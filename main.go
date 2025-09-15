a2a66de6f3b39841a6e02fdd2738ca977e479aba
123zengyue <3229587344@qq.com>
2025-09-15 23:20:52 +0800

    函数闭包实现fibonacci

--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import "fmt"
 
-// 函数闭包，当作出参的函数，在其调用域的内部 ,维护了一个外部环境 sum
-func add() func(int) int {
-	sum := 0
-	return func(x int) int {
-		sum += x
-		return sum
+// fibonacci 是返回一个「返回一个 int 的函数」的函数
+func fibonacci() func() int {
+	pre := 0
+	end := 1
+	return func() int {
+		result := pre
+		pre, end = end, pre+end
+		return result
 	}
 }
 
 func main() {
-	pos, neg := add(), add()
+	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(i, pos(i), neg(-1*i))
+		fmt.Println(f())
 	}
 }
